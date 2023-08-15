217075347cada1d9568623fb3a7e17c23fd80cb7
mahmoudmatar <mahmoudmatar49@gmail.com>
2023-08-15 13:30:32 -0700

    change some thing

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,19 @@ public class Main {
     public static void main(String[] args) {
         Thread.setDefaultUncaughtExceptionHandler(new GlobalExceptionHandler());
 
+        String s = "";
+        s.charAt(5);
 
-
-        try {
-//            int x = 3 / 0;
-            String s = "";
-            s.charAt(5);
-        }catch (StringIndexOutOfBoundsException e){
-            System.out.println(e.getMessage());
-        }catch (RuntimeException e) {
-            System.out.println("RunTime");
-        } catch (Exception e) {
-            System.out.println("Exception");
-        }
+//        try {
+////            int x = 3 / 0;
+//            String s = "";
+//            s.charAt(5);
+//        }catch (StringIndexOutOfBoundsException e){
+//            System.out.println(e.getMessage());
+//        }catch (RuntimeException e) {
+//            System.out.println("RunTime");
+//        } catch (Exception e) {
+//            System.out.println("Exception");
+//        }
     }
 }
